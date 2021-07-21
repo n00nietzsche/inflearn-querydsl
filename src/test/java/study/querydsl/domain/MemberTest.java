9ef7c5f2ed5457ef09d23658cb74d32995fa558f
n00nietzsche <n00nietzsche@gmail.com>
2021-07-21 22:10:50 +0900

    실전! QueryDSL #4 예제 도메인 모델과 동작 확인

---
+++ src/test/java/study/querydsl/domain/MemberTest.java
@@ -0,0 +1,62 @@
+package study.querydsl.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Commit
+class MemberTest {
+    @Autowired EntityManager em;
+
+    Team teamA;
+    Team teamB;
+
+    Member member1;
+    Member member2;
+    Member member3;
+    Member member4;
+
+    @BeforeEach
+    public void makeFixture() {
+        teamA = new Team("teamA");
+        teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        member1 = new Member("member1", 10, teamA);
+        member2 = new Member("member2", 20, teamA);
+        member3 = new Member("member3", 30, teamB);
+        member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+    }
+
+    @Test
+    public void testEntity() {
+        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("member.getTeam() = " + member.getTeam());
+        }
+    }
+}
